8380c37c30146ab11f3ed1f51f908ac7af8d2abe
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2026-04-16 21:50:42 -0300

    Mudanças

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const app = express();
 
 // Importação das rotas de livros,usuarios e emprestimos
 const rotaLivros = require('./src/routes/livros');
-//const rotaUsuarios = require('./src/routes/usuarios');
-//const rotaEmprestimos = require('./src/routes/emprestimos');
+const rotaUsuarios = require('./src/routes/usuarios');
+const rotaEmprestimos = require('./src/routes/emprestimos');
 
 app.use(express.json());
 
 // Definição dos prefixos das rotas
 app.use('/livros', rotaLivros);
-//app.use('/usuarios', rotaUsuarios);
-//app.use('/emprestimos', rotaEmprestimos);
+app.use('/usuarios', rotaUsuarios);
+app.use('/emprestimos', rotaEmprestimos);
 
 //Rodando o Servidor
 const PORT = 3000;
